0400add110d1e3ee6415b5efc4922e4a81e804bf
Kamil Śliwak <kamil.sliwak@codepoets.it>
2022-08-05 22:32:37 +0200

    fixup! Fix wrong cleanup when copying from calldata to memory

--- test/libsolidity/semanticTests/abiEncoderV2/memory_dynamic_array_and_calldata_bytes.sol
+++ test/libsolidity/semanticTests/abiEncoderV2/memory_dynamic_array_and_calldata_bytes.sol
@@ -1,5 +1,3 @@
-pragma abicoder v2;
-
 contract C {
     function f(uint256[] memory a, bytes calldata b) public returns (bytes memory) {
         return abi.encode(a, b);
@@ -10,8 +8,6 @@ contract C {
     }
 }
 
-// ====
-// EVMVersion: >homestead
 // ----
 // f(uint256[],bytes): 0x40, 0x80, 1, 0xFF, 6, "123456" -> 0x20, 0xc0, 0x40, 0x80, 1, 0xff, 6, "123456"
 // g(uint256[],bytes): 0x40, 0x80, 1, 0xffff, 8, "12345678" -> 0x20, 0xc0, 0x40, 0x80, 1, 0xffff, 8, "12345678"
